9dbbc66de5e1d07fdb28c9f44eb3ab5061035ec1
egginsect <birdegg1357@gmail.com>
2014-06-11 22:34:39 +0800

    apply cvxpy functions

--- terminal.py
+++ terminal.py
@@ -17,6 +17,14 @@ class Device(Terminal):
 class Generator(Device):
     def __init__(self, name, connections):
         Device.__init__(self, name, 'G', connections)
+        self.params = {'alpha':1,'beta':1, 'Cmax':1}
+    def objective_function(p):
+        return self.params['alpha']*cvx.sum_squares(p)+self.params['beta']*sum_entries(p)
+    def constrain(p):
+        con = list()
+        for i in range(1,len(p)):
+            con.append(p[i+1,:]-p[i,:]<=self.params['Cmax'])
+        return con
        
 class Battery(Device):
     def __init__(self, name, connections):
