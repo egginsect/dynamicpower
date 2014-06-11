9398a80a7afe23184df52afac3ebc6e6b9b95f2c
egginsect <birdegg1357@gmail.com>
2014-06-11 10:39:18 +0800

    modify addterminal

--- powersystem.py
+++ powersystem.py
@@ -1,4 +1,5 @@
 from terminal import *
+from multiprocessing import Pool
 class PowerSystem(object):
     def __init__(self, terminals, adjmat):
        self.devicelist = list()
@@ -8,22 +9,22 @@ class PowerSystem(object):
        self.adddterminals(terminals)
 
     def adddterminals(self, terminals):
-        for terminal in terminals:
+        for (terminal,neighbors) in zip(terminals, self.adjmat.tolist()):
             if terminal not in self.terminal_count.keys():
                 print 'Wrong terminal type' 
             else:
                 if terminal is 'N':
-                    self.netlist.append(Net('N'+str(sum(self.terminal_count.values())),None))
+                    self.netlist.append(Net('N'+str(self.terminal_count[terminal]), neighbors))
                 elif terminal is 'G':
-                    self.devicelist.append(Generator('G'+str(sum(self.terminal_count.values())),None))
+                    self.devicelist.append(Generator('G'+str(self.terminal_count[terminal]), neighbors))
                 elif terminal is 'B':
-                    self.devicelist.append(Battery('B'+str(sum(self.terminal_count.values())),None))
+                    self.devicelist.append(Battery('B'+str(self.terminal_count[terminal]), neighbors))
                 elif terminal is 'T':
-                    self.devicelist.append(TransmissionLine('T'+str(sum(self.terminal_count.values())),None))
+                    self.devicelist.append(TransmissionLine('T'+str(self.terminal_count[terminal]), neighbors))
                 elif terminal is 'DL':
-                    self.devicelist.append(DefferableLoad('DL'+str(sum(self.terminal_count.values())),None))
+                    self.devicelist.append(DefferableLoad('DL'+str(self.terminal_count[terminal]), neighbors))
                 elif terminal is 'CL':
-                    self.devicelistnetlist.append(CurtalibleLoad('CL'+str(sum(self.terminal_count.values())),None))
+                    self.devicelistnetlist.append(CurtalibleLoad('CL'+str(self.terminal_count[terminal]), neighbors))
                 self.terminal_count[terminal]+=1
             
    
