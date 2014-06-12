4af87abbd6502ee8408891593e12950e40890010
egginsect <birdegg1357@gmail.com>
2014-06-12 20:34:59 +0800

    cvxpy in generator

--- deviceTest.py
+++ deviceTest.py
@@ -1,6 +1,8 @@
 from terminal import * 
 import pdb
 g = Generator('G1',None)
-b = Battery('G1',None)
+#b = Battery('G1',None)
+p = g.solve_problem(1)
+print p
 pdb.set_trace()
 
--- terminal.py
+++ terminal.py
@@ -13,18 +13,27 @@ class Device(Terminal):
     def __init__(self, name, device_type, connections):
         Terminal.__init__(self, name, 'Device', connections)
         self.device_type = device_type
+        self.T = 10 
 
 class Generator(Device):
     def __init__(self, name, connections):
         Device.__init__(self, name, 'G', connections)
         self.params = {'alpha':1,'beta':1, 'Cmax':1}
-    def objective_function(p):
-        return self.params['alpha']*cvx.sum_squares(p)+self.params['beta']*sum_entries(p)
-    def constrain(p):
+        self.p =None
+    def cost_function(self,p):
+        return self.params['alpha']*cvx.sum_squares(p)+self.params['beta']*cvx.sum_entries(p)
+    def constrain(self,p):
         con = list()
-        for i in range(1,len(p)):
-            con.append(p[i+1,:]-p[i,:]<=self.params['Cmax'])
+        for i in range(1, p.size[0]):
+            con.append(p[i,:]-p[i-1,:]<=self.params['Cmax'])
         return con
+    def solve_problem(self,u):
+        p = cvx.Variable(self.T)
+        objective = cvx.Minimize(self.cost_function(p))
+        constraints = self.constrain(p)
+        prob = cvx.Problem(objective, constraints)
+        prob.solve(verbose=True)
+        self.p = p.value   
        
 class Battery(Device):
     def __init__(self, name, connections):
